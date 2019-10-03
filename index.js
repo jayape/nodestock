47842d89b55d2e9b44884c578623a41d6a47b32e
John Pertell <jpertell@gmail.com>
2019-10-03 12:03:52 -0500

    Added API Stock Data

--- index.js
+++ index.js
@@ -4,21 +4,39 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
 
-// Set HAndlebars Middleware
+
+// API KEY pk_2e8af6c4ffc14edca46fc4b1d6d1de67
+// Create API call function
+function call_api(finishedAPI) {
+	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_2e8af6c4ffc14edca46fc4b1d6d1de67', {json: true}, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
+
+
+// Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
 
 const otherstuff = 'Hello there, this is other stuff';
 
 // Set handlebars routes
 app.get('/', function (req, res) {
-	res.render('home', {
-		stuff: otherstuff
+	call_api(function(doneAPI) {
+		res.render('home', {
+			stock: doneAPI
+		});		
 	});
+	
 });
 
 // Set about page routes
